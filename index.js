081d1a52a724ed1bd5c068f835fea4d01eba902a
arsalanash <arsalanashraf562562@gmail.com>
2024-08-13 15:12:48 +0530

    cors changed

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
-
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO)
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch((err) => console.log(err));
 
 const __dirname = path.resolve();
 
@@ -23,34 +22,39 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'https://arsalan-estate-frontend.vercel.app' }));
+app.use(
+    cors({
+        origin: "https://arsalan-estate-frontend.vercel.app",
+        credentials: true, // Allow cookies to be sent from the frontend
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    })
+);
 
 // API Routes
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // Serve React app
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
+    console.log(`Server is running on port ${PORT}!`);
 });
-
